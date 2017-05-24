0f912db080450aba3c448735b4f83319ef556a52
DickyMP <dicky.muhammad.priangga15@mhs.if.its.ac.id>
2017-05-24 08:54:29 +0700

    Add files via upload

---
+++ sbmptn/navbar_head.php
@@ -0,0 +1,37 @@
+	<table align="center" valign="top" cellSpacing="0" cellPadding="0" border="0" width="100%">
+		<tr>
+			<td width="100%" height="189">
+				<table width="100%" height="100%" align="center" valign="top" cellSpacing="0" cellPadding="0" border="0">
+					<tr>
+						<td align="center" colspan="3">
+							<table width="100%" height="100%" align="center" valign="top" cellSpacing="0" cellPadding="0" border="0">
+								<tr bgcolor="#201059">
+									<td align="left" valign="middle" bgcolor="#2e72b6" height="100" width="100" >
+										<img src="./images/logosbmptn.gif">
+									</td>
+									<td align="left" valign="middle" bgcolor="#2e72b6" >
+										<font face="Tahoma,Verdana,Geneva,Arial" size="3" color="#ffffff">Laman Resmi</font><br>
+										<font face="Arial" size="10" color="#ffffff"><b>SBMPTN</b></font>
+										<font face="Arial" size="10" color="#ff871b"><b>2017</b></font><br>
+										<font face="Tahoma,Verdana,Geneva,Arial" size="3" color="#ffffff">Seleksi Bersama Masuk Perguruan Tinggi Negeri Tahun 2017</font>
+									</td>
+									<td align="right" valign="middle" bgcolor="#ffffff" width="340" bgcolor="#2e72b6" >
+										<img src="./images/bank.gif"><img src="./images/telkom_baru2.gif">
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+					<tr bgcolor="#bbbbbb" height="3">
+						<td colspan="4">
+						</td>
+					</tr>
+					<tr bgcolor="#201059">
+						<td align="left" valign="middle" height="30" id="running" style="FONT: bold 12px 'Courier New', Courier, monotype, fixedsys, monospace, terminal;   COLOR: #fcfd0b">
+							<marquee>PANLOK 11 menyelenggarakan ujian keterampilan Olah Raga, Seni Rupa, Sendratasik, Seni Tari, dan Seni Musik di Universitas Syiah Kuala | Waktu Pembayaran biaya seleksi DIPERPANJANG sampai pada hari Senin, 8 Mei 2017 pukul 16.00 WIB. | Penuntasan Pendaftaran SBMPTN 2017 DIPERPANJANG sampai pada hari Selasa, 9 Mei 2017 pukul 16.00 WIB.</marquee>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
---
+++ sbmptn/template.php
@@ -0,0 +1,8 @@
+<? php
+	include 'header.php';
+?>
+<body>
+<? php
+	include 'navbar_head.php';
+?>
+</body>
